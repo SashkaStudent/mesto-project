feb1786e241757a75b71b7eba101af440ca70a38
Aleksandr Stepanenko <alalstepan@gmail.com>
2022-10-17 19:58:43 +1200

    feat: class popup

---
+++ src/components/Popup.js
@@ -0,0 +1,32 @@
+export class Popup {
+  constructor(selector) {
+    this._popup = document.querySelector(selector);
+  }
+
+  open() {
+    this._popup.classList.add("popup_opened");
+      document.addEventListener("keydown", (evt) => { this._handleEscClose(evt) });
+  }
+
+  close() {
+    this._popup.classList.remove("popup_opened");
+    document.removeEventListener("keydown", (evt) => { this._handleEscClose(evt) });
+  }
+
+    _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popup.addEventListener("click", (evt) => {
+      if (
+        evt.target.classList.contains("popup") ||
+        evt.target.classList.contains("popup__close-button")
+      ) {
+        this.close();
+      }
+    });
+  }
+}
